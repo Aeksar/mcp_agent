4592e4bb60f9135a7fa6cbcf099ceb3e9e6c082b
Aeksar <skoshkidko12345@mail.ru>
2025-10-18 00:35:33 +0300

    feat: add catch model exception

--- app/bots/mcp_router.py
+++ app/bots/mcp_router.py
@@ -1,5 +1,6 @@
 from aiogram import Router, types
 from aiogram.enums import ParseMode
+from httpx import HTTPStatusError
 
 from app.utils.escape import escape_markdown
 from app.agent.agent import get_agent
@@ -10,8 +11,11 @@ router = Router(name="mcp_handler")
 
 @router.message(lambda x: x == x)
 async def mcp_handler(message: types.Message) -> None:
-    agent = await get_agent()
-    config = {"configurable": {"session_id": str(message.chat.id)}}
-    result = await agent.ainvoke({"input": message.text}, config)
-    answer = escape_markdown(result["output"])
-    await message.answer(answer, parse_mode=ParseMode.MARKDOWN_V2)
+    try:
+        agent = await get_agent()
+        config = {"configurable": {"session_id": str(message.chat.id)}}
+        result = await agent.ainvoke({"input": message.text}, config)
+        answer = escape_markdown(result["output"])
+        await message.answer(answer, parse_mode=ParseMode.MARKDOWN_V2)
+    except HTTPStatusError as e:
+        await message.answer("Модель перегружена\nОтправте свое сообщение позже")
